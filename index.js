f61c31036486f4f2f788019a26048d3c5960b255
sooraj_s <sooraj1998ayiroor@gmail.com>
2020-07-21 19:44:55 +0530

    Mongoose Part 3

--- index.js
+++ index.js
@@ -8,24 +8,40 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
 
     console.log('Connected correctly to server');
-
-    var newDish = Dishes({
+    Dishes.create({
         name: 'Uthappizza',
         description: 'test'
-    }).then((dish) => {
-            console.log(dish);
+    })
+    .then((dish) => {
+        console.log(dish);
 
-            return Dishes.find({});
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
         })
-        .then((dishes) => {
-            console.log(dishes);
+        .exec();
+    })
+    .then((dish) => {
+        console.log(dish);
 
-            return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
         });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 });
